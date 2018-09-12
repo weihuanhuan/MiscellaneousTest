694d70fdf862fffe403282d003273e2989154ec3
weihuanhuan1107 <weihuanhuan1107@vip.qq.com>
2018-09-12 10:40:03 +0800

    regex capturing group

--- src/main/java/regularexpression/RegularExpression.java
+++ src/main/java/regularexpression/RegularExpression.java
@@ -2,33 +2,32 @@ package regularexpression;
 
 import java.util.Arrays;
 import java.util.EmptyStackException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by JasonFitch on 1/29/2018.
  */
-public class RegularExpression
-{
-    public static void main(String[] args)
-    {
+public class RegularExpression {
+    public static void main(String[] args) {
 
         String str2 = "1, 0,\n2,\n3,\n,";
         String[] result2 = str2.split("(,$)");
-        System.out.println(Arrays.deepToString(result2)+result2.length);
+        System.out.println(Arrays.deepToString(result2) + result2.length);
         System.out.println("-----------------------------------------------------");
         String str3 = "1, 0,\r\n2,\r\n3,\r\n,";
         String[] result3 = str3.split("(,$)");
-        System.out.println(Arrays.deepToString(result3)+result3.length);
+        System.out.println(Arrays.deepToString(result3) + result3.length);
         System.out.println("-----------------------------------------------------");
 //      \r\n and \n is not a end of line for RegularExpression
 
         String str1 = "1, 0,\r\n2,\r\n3,\r\n,";
         String[] result1 = str1.split("(,\\n)|(,\\r\\n)");
-        System.out.println(Arrays.deepToString(result1)+result1.length);
-        for(int i=0; i<result1.length;++i)
-        {
-            result1[i]=result1[i].replaceAll("(^\\s+)|(\\s+$)|(,$)","");
+        System.out.println(Arrays.deepToString(result1) + result1.length);
+        for (int i = 0; i < result1.length; ++i) {
+            result1[i] = result1[i].replaceAll("(^\\s+)|(\\s+$)|(,$)", "");
         }
-        System.out.println(Arrays.deepToString(result1)+result1.length);
+        System.out.println(Arrays.deepToString(result1) + result1.length);
         System.out.println("-----------------------------------------------------");
 //      deal with dos and unix linefeed at the same time
 
@@ -43,6 +42,22 @@ public class RegularExpression
         System.out.println("-----------------------------------------------------");
 
         System.out.println(System.getProperty("java.class.path"));
+
+
+        System.out.println("-----------------------------------------------------");
+        String jvmStr = "JVM_OPTIONS=%JVM_OPTIONS% -Dkey=value";
+        String regex = ".*JVM_OPTIONS=.*JVM_OPTIONS.*\\s+-D(.*)=(.*)";
+        boolean isMatch = jvmStr.matches(regex);
+        System.out.println(isMatch);
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(jvmStr);
+        if (matcher.find()) {
+            int count = matcher.groupCount();
+            System.out.println(matcher.groupCount());
+            for (int i = 0; i < count+1;++i)
+            System.out.println(matcher.group(i));
+        }
 
     }
 }
