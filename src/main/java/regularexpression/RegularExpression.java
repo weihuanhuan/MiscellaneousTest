70818b03fcb0a08d31d6a1187c1d9be90f52609b
weihuanhuan1107 <weihuanhuan1107@vip.qq.com>
2018-10-19 15:40:11 +0800

    regex shy groups

--- src/main/java/regularexpression/RegularExpression.java
+++ src/main/java/regularexpression/RegularExpression.java
@@ -16,17 +16,17 @@ public class RegularExpression {
 
     public static void main(String[] args) {
 
-        String   str2    = "1, 0,\n2,\n3,\n,";
+        String str2 = "1, 0,\n2,\n3,\n,";
         String[] result2 = str2.split("(,$)");
         System.out.println(Arrays.deepToString(result2) + result2.length);
         System.out.println("-----------------------------------------------------");
-        String   str3    = "1, 0,\r\n2,\r\n3,\r\n,";
+        String str3 = "1, 0,\r\n2,\r\n3,\r\n,";
         String[] result3 = str3.split("(,$)");
         System.out.println(Arrays.deepToString(result3) + result3.length);
         System.out.println("-----------------------------------------------------");
 //      \r\n and \n is not a end of line for RegularExpression
 
-        String   str1    = "1, 0,\r\n2,\r\n3,\r\n,";
+        String str1 = "1, 0,\r\n2,\r\n3,\r\n,";
         String[] result1 = str1.split("(,\\n)|(,\\r\\n)");
         System.out.println(Arrays.deepToString(result1) + result1.length);
         for (int i = 0; i < result1.length; ++i) {
@@ -50,23 +50,36 @@ public class RegularExpression {
 
 
         System.out.println("-------------jvmStr---------------------");
-        String  jvmStr1  = "JVM_OPTIONS=%JVM_OPTIONS% -Dkey=value";
-        String  regex1  = ".*JVM_OPTIONS=.*JVM_OPTIONS.*\\s+-D(.*)=(.*)";
-        //尤其贪婪匹配,会将最后一个等号之前的内容全部捕获。
-        String  jvmStr2  = "JVM_OPTIONS=%JVM_OPTIONS% -Dkey=value=";
-        String  regex2   = ".*JVM_OPTIONS=.*JVM_OPTIONS.*\\s+-D(((?!=).)*)=(.*)";
-        //使用正向否定预查，将不包括 = 的第一个捕获组取出。
-        boolean isMatch = jvmStr1.matches(regex1);
-        System.out.println(isMatch);
 
-        Pattern pattern = Pattern.compile(regex1);
-        Matcher matcher = pattern.matcher(jvmStr2);
-        //注意将 regex1 来匹配 jvmStr2 时匹配的内容
+        String sysPropStrWithoutEqual = "JVM_OPTIONS=%JVM_OPTIONS% -Dkey=value";
+        String sysPropRegexGreedy = ".*JVM_OPTIONS=.*JVM_OPTIONS.*\\s+-D(.*)=(.*)";
+        //尤其贪婪匹配(greedy),会将最后一个等号之前的内容全部捕获。
+        String sysPropStrWithEqual = "JVM_OPTIONS=%JVM_OPTIONS% -Dkey=value=";
+        String sysPropRegexLANA = ".*JVM_OPTIONS=.*JVM_OPTIONS.*\\s+-D(((?!=).)*)=(.*)";
+        //使用正向否定预查(look ahead negative assert)，将不包含第一个 = 之前的内容取出。
+//        同时注意这里捕获了 3 个分组，第一个是key，第二个是 = 前的最后一个字符，第三个是value
+        boolean isMatch = sysPropStrWithoutEqual.matches(sysPropRegexGreedy);
+        System.out.println("###without equal:"+isMatch);
+
+        String jvmOptionStrNormal = "JVM_OPTIONS=%JVM_OPTIONS% -server";
+        String jvmOptionStrWithD = "JVM_OPTIONS=%JVM_OPTIONS% -ser-Dver";
+        String jvmOptionRegexGreedy = ".*JVM_OPTIONS=.*JVM_OPTIONS.*\\s+((?!-D).*)";
+        //注意贪婪匹配(greedy),会将最后一个空格之前的内容全部捕获。
+//        这里的捕获分组是 1 个 ，就是最后一个空格之后的内容。
+        String jvmOptionStrWithSpace = "JVM_OPTIONS=%JVM_OPTIONS% -ser ver";
+        String jvmOptionRegexLANA = ".*JVM_OPTIONS=.*JVM_OPTIONS(?:(?:(?!\\s).)*)\\s+((?!-D).*)";
+//        通过对每一个字符检查，确保不匹配和 \s 在前面的字符，然后通过非捕获匹配 ( shy group )忽略其组号占用。
+        isMatch = jvmOptionStrWithD.matches(jvmOptionRegexGreedy);
+        System.out.println("###with -D:"+isMatch);
+
+        Pattern pattern = Pattern.compile(jvmOptionRegexLANA);
+        Matcher matcher = pattern.matcher(jvmOptionStrWithSpace);
+        //注意将 sysPropRegexGreedy 来匹配 sysPropStrWithEqual 时匹配的内容
         if (matcher.find()) {
             int count = matcher.groupCount();
-            System.out.println(matcher.groupCount());
+            System.out.println("###groupCount:"+matcher.groupCount());
             for (int i = 0; i < count + 1; ++i)
-                System.out.println("i="+i+":"+matcher.group(i));
+                System.out.println("i=" + i + ":" + matcher.group(i));
         }
 
 
@@ -74,13 +87,13 @@ public class RegularExpression {
         StringBuffer stringBuffer = new StringBuffer();
         matcher.reset();
         while (matcher.find()) {
-                matcher.appendReplacement(stringBuffer,matcher.group().toUpperCase());
+            matcher.appendReplacement(stringBuffer, matcher.group().toUpperCase());
         }
         matcher.appendTail(stringBuffer);
         System.out.println(stringBuffer.toString());
 
         System.out.println("-------------Scanner---------------------");
-        Scanner scanner = new Scanner(new BufferedReader(new StringReader(jvmStr1)));
+        Scanner scanner = new Scanner(new BufferedReader(new StringReader(sysPropStrWithoutEqual)));
         scanner.useDelimiter(System.lineSeparator());
         //注意修改定界符，因为Scanner只会依据 定界符 来读取下一个分词，
         // 如果正则表达式中含有定界符的匹配，那么Scanner 的 hasNext() 方法将永远返回false
@@ -89,6 +102,6 @@ public class RegularExpression {
             MatchResult matchResult = scanner.match();
             System.out.println(matchResult.group());
         }
-        
+
     }
 }
