149e79e711813dbc0ca1a988be4c5f467e815838
weihuanhuan1107 <weihuanhuan1107@vip.qq.com>
2018-10-22 17:25:13 +0800

    jdk jre lib

--- src/main/java/SystemTest/SystemTest.java
+++ src/main/java/SystemTest/SystemTest.java
@@ -14,7 +14,12 @@ public class SystemTest {
         System.out.println("------------------------------------------------");
         String userdir = System.getProperty("user.dir");
         System.out.println("#####user.dir:"+userdir);
+//        当前工作目录，用户的家目录是user.home
+        String JAVA_HOME = System.getenv("JAVA_HOME");
+        System.out.println("#####JAVA_HOME:"+JAVA_HOME);
+//        环境变量的JAVA_HOME是系统设置的。
         String javahome = System.getProperty("java.home");
+//        而要注意java.home的输出是JAVA_HOME/jre, 这里是jre的lib，不是jdk的lib，他们并不相同
         System.out.println("#####java.home:"+javahome);
 
         System.setProperty("java.home","C:/java");
