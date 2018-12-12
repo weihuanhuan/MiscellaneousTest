6b9437abad68bde8368e78ce88f30e1362dcaad3
weihuanhuan1107 <weihuanhuan1107@vip.qq.com>
2018-12-12 20:16:46 +0800

    log4j2

--- src/main/java/log/log4j2/Log4j2Test.java
+++ src/main/java/log/log4j2/Log4j2Test.java
@@ -1,5 +1,7 @@
 package log.log4j2;
 
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -10,15 +12,25 @@ public class Log4j2Test {
 
     static {
 //        System.setProperty("log4j2.debug'","true");
-        System.setProperty("Log4jContextSelector","org.apache.logging.log4j.core.async.AsyncLoggerContextSelector");
+        System.setProperty("Log4jContextSelector", "org.apache.logging.log4j.core.async.AsyncLoggerContextSelector");
     }
 
-    private static Logger logger = LogManager.getLogger("LoggerName");
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
 
-        System.out.println(logger.toString());
-        logger.error("Hello, World!");
+        Logger notRootLogger1 = LogManager.getLogger("NotRootLogger1");
+        notRootLogger1.error("Hello notRootLogger1, level error!");
+
+        Logger notRootLogger2 = LogManager.getLogger("NotRootLogger2");
+        notRootLogger2.error("Hello notRootLogger2, level error!");
+
+        Logger notRootLogger3 = LogManager.getLogger("NotRootLogger3");
+        notRootLogger3.error("Hello notRootLogger3, level error!");
+
+        //3个AsyncLogger+1个RootLogger，只有2个AsyncLogger线程，那么他们之间关系如何？
+
+        // 默认配置下异步会丢失某些日志，延时让程序写完日志事件
+//        TimeUnit.MILLISECONDS.sleep(6000 * 3600);
 
     }
 }
