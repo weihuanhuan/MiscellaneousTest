8e7d6319fc2b003ae292cc1dfc01f4c666fe489f
weihuanhuan1107 <weihuanhuan1107@126.com>
2019-02-14 17:47:15 +0800

    redis auth

---
+++ src/main/java/redis/RedisWithRequirePass.java
@@ -0,0 +1,21 @@
+package redis;
+
+import redis.clients.jedis.Jedis;
+
+/**
+ * Created by JasonFitch on 2/14/2019.
+ */
+public class RedisWithRequirePass {
+
+    public static void main(String[] args) {
+
+        Jedis jedis;
+        jedis = new Jedis("192.168.88.10", 6379);
+        jedis.auth("123456");
+        jedis.set("foo", "bar");
+        String value = jedis.get("foo");
+        System.out.println(value);
+
+    }
+
+}
