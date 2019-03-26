dc806a0ccf8738c259f35c7c6060d247f182a12b
weihuanhuan1107 <weihuanhuan1107@126.com>
2019-03-26 14:27:09 +0800

    system out and err redirect

--- src/main/java/SystemTest/Redirect/RedirectTest.java
+++ src/main/java/SystemTest/Redirect/RedirectTest.java
@@ -9,7 +9,7 @@ import java.io.PrintStream;
  */
 public class RedirectTest {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         System.out.println("std console");
 
@@ -20,7 +20,26 @@ public class RedirectTest {
             }
         }));
 
-        System.out.println("after redirect");
-        //此句不会输出，因为标准输出的写动作，已经在重写时哑掉了。
+        String info = "after redirect";
+        //JF 此句不会输出，因为标准输出的写动作，已经在重写时哑掉了。
+        System.out.println(info+" from out println");
+        //println底层调用的wirte
+        System.out.write((info+" from out write").getBytes());
+
+        //err流独立于out流
+        System.err.println(info+" frlnom err println");
+        System.err.write((info+" from err write").getBytes());
+
+        //JF 注意 err 与 out 是俩个不同的输出地，其操作是独立的，所以他们之间不能保证输出的顺序问题，
+        //   但是 同一个流内部的顺序是可以确定的，在任务顺序确定时。
+//        std console
+//        after redirect frlnom err println
+//        after redirect from err write
+
+//        after redirect frlnom err println
+//        std console
+//        after redirect from err write
+
+
     }
 }
