7ffe63e076f6dca74ed373d17e4b5a85fee52d07
weihuanhuan1107 <weihuanhuan1107@126.com>
2019-04-22 14:19:02 +0800

    file lastModified and instanceOf null

--- src/main/java/FileTest/FileTest.java
+++ src/main/java/FileTest/FileTest.java
@@ -25,6 +25,11 @@ public class FileTest {
             } else {
                 System.out.println("not exist");
             }
+
+            //JF 该值是文件的实时值，如果文件在创建该变量之后被删除或者改名了，那么其值为 0
+            //   所以要在备份文件时利用该值确定文件版本，需要在文件改名前就暂存其该值。
+            long lastModified = pathTest.lastModified();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
---
+++ src/main/java/TypeIdentifiedTest/InstanceOfTest.java
@@ -0,0 +1,15 @@
+package TypeIdentifiedTest;
+
+/**
+ * Created by JasonFitch on 4/16/2019.
+ */
+public class InstanceOfTest {
+
+
+    public static void main(String[] args) {
+
+        //JF 对于值为null的对象可以直接判断其类型
+        System.out.println(null instanceof  Exception);
+
+    }
+}
