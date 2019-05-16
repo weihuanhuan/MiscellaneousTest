cadaf562653294e279868eed2d2c0902366ea64b
weihuanhuan1107 <weihuanhuan1107@126.com>
2019-05-16 17:21:25 +0800

    meta-annotation simple demo

---
+++ feature/src/main/java/annotation/anno/AnnoC.java
@@ -0,0 +1,20 @@
+package annotation.anno;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by JasonFitch on 5/16/2019.
+ */
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@AnnoB("AnnoB")
+public @interface AnnoC {
+
+    String value();
+}
