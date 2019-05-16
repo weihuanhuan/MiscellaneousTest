921c331f0232e5edb56f74f983fb5d3c72508e5c
weihuanhuan1107 <weihuanhuan1107@126.com>
2019-05-16 17:21:25 +0800

    meta-annotation simple demo

---
+++ feature/src/main/java/annotation/clazz/A.java
@@ -0,0 +1,10 @@
+package annotation.clazz;
+
+import annotation.anno.AnnoA;
+
+/**
+ * Created by JasonFitch on 5/16/2019.
+ */
+@AnnoA("AnnoA")
+public class A {
+}
---
+++ feature/src/main/java/annotation/clazz/D.java
@@ -0,0 +1,10 @@
+package annotation.clazz;
+
+import annotation.anno.AnnoD;
+
+/**
+ * Created by JasonFitch on 5/16/2019.
+ */
+@AnnoD("AnnoD")
+public class D {
+}
