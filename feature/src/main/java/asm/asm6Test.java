7b5ab12ddd84117a8ea60ab3015ba49f86fbf522
weihuanhuan1107 <weihuanhuan1107@126.com>
2019-09-06 15:35:22 +0800

    relationship between asm-all and asm sub project

---
+++ feature/src/main/java/asm/asm6Test.java
@@ -0,0 +1,22 @@
+package asm;
+
+/**
+ * Created by JasonFitch on 9/6/2019.
+ */
+public class asm6Test {
+
+
+    public static void main(String[] args) {
+
+
+        System.out.println();
+//        <module>asm-all</module>
+//        asm-all 是指将下列 asm相关子项目的全部 classs文件 打包到一起的 fat-jar
+
+//        <module>asm</module>
+//        <module>asm-analysis</module>
+//        <module>asm-commons</module>
+//        <module>asm-tree</module>
+
+    }
+}
