ebfb4965316f957e0cabc4ad1dde03993b74525b
weihuanhuan1107 <weihuanhuan1107@126.com>
2019-10-09 19:39:30 +0800

    logger name null

--- feature/src/main/java/log/jdk/JDKLoggerTest.java
+++ feature/src/main/java/log/jdk/JDKLoggerTest.java
@@ -16,5 +16,21 @@ public class JDKLoggerTest {
 
         //JF Exception in thread "main" java.lang.NullPointerException
         Logger loggerNull = Logger.getLogger(null);
+
+        InterLogger interLogger = new InterLogger(null, null);
+        System.out.println(interLogger.getName()==null);
+        //JF java.lang.NullPointerException，注意日志记录器的 name 可以为 null ,
+        //   故最好把常量放在表达式的前面，防止不必要的NPE问题
+        if("".equals(interLogger.getName())){
+            System.out.println("logger name test for null");
+        }
+
+    }
+
+    static class InterLogger extends Logger {
+
+        protected InterLogger(String name, String resourceBundleName) {
+            super(name, resourceBundleName);
+        }
     }
 }
