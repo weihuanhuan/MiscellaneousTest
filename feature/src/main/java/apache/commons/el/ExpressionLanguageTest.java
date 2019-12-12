0a850588c641e361bb663ebfaec5c04393c64428
weihuanhuan1107 <weihuanhuan1107@126.com>
2019-12-12 19:31:34 +0800

    el (expression language) maven dependency

---
+++ feature/src/main/java/apache/commons/el/ExpressionLanguageTest.java
@@ -0,0 +1,14 @@
+package apache.commons.el;
+
+/**
+ * Created by JasonFitch on 12/12/2019.
+ */
+public class ExpressionLanguageTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("el test");
+
+    }
+
+}
