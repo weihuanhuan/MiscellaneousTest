dbe609b085c5f1064d60b1425ed810f2082f5300
weihuanhuan1107 <weihuanhuan1107@126.com>
2020-05-16 18:13:39 +0800

    explicit null variable type test instanceof

--- feature/src/main/java/TypeIdentifiedTest/InstanceOfTest.java
+++ feature/src/main/java/TypeIdentifiedTest/InstanceOfTest.java
@@ -11,5 +11,14 @@ public class InstanceOfTest {
         //JF 对于值为null的对象可以直接判断其类型
         System.out.println(null instanceof  Exception);
 
+
+        //JF 即使显式的指定对象的类型，然后赋值为 null , 也不会通过 instanceof 运算符的测试
+        String nullString = null;
+        if (nullString instanceof String) {
+            System.out.println("nullString instanceof String");
+        } else {
+            System.out.println("nullString isn't instanceof String");
+        }
+
     }
 }
