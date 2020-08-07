a969f9bd09282937cadaf7ac370799ee4ca18f76
weihuanhuan1107 <weihuanhuan1107@126.com>
2020-08-07 19:44:05 +0800

    javascript syntax assignment test

---
+++ springmvc/web/js/syntaxAssignment.js
@@ -0,0 +1,32 @@
+/**
+ * Created by JasonFitch on 8/7/2020.
+ */
+
+function syntaxAssignmentTest() {
+
+    var value = 0;
+
+    alert(!value++);
+    alert(value);
+
+    if (0) {
+        alert(0);
+    }
+
+    if (!1) {
+        alert(!1);
+    }
+
+    if (2) {
+        alert(2);
+    }
+
+    if (false) {
+        alert(false);
+    }
+
+    if (true) {
+        alert(true);
+    }
+
+}
