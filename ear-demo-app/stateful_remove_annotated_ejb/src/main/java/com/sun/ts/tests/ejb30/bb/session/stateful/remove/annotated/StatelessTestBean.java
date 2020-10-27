146de4d5a3041025f6904df2552fd0d0de138e86
weihuanhuan1107 <weihuanhuan1107@126.com>
2020-10-27 16:53:20 +0800

    ear-demo-app: ejb remote home handle test from testcase stateful_remove_annotated of cts, tomee ejb remote invoke with tcp and http.

--- ear-demo-app/stateful_remove_annotated_ejb/src/main/java/com/sun/ts/tests/ejb30/bb/session/stateful/remove/annotated/StatelessTestBean.java
+++ ear-demo-app/stateful_remove_annotated_ejb/src/main/java/com/sun/ts/tests/ejb30/bb/session/stateful/remove/annotated/StatelessTestBean.java
@@ -79,10 +79,17 @@ public class StatelessTestBean extends TestBeanBase implements TestIF {
     @Override
     public RemoveIF getRemoveRemoteBeanByRemoteCtxReturn() throws NamingException {
         Properties properties = new Properties();
-        properties.put(Context.PROVIDER_URL, "http://127.0.0.1:8080/tomee/ejb");
-        properties.put(Context.INITIAL_CONTEXT_FACTORY, "org.apache.openejb.client.RemoteInitialContextFactory");
-        properties.setProperty("openejb.client.moduleId", "openejb/global");
 
+        //这里的 url 使用了 tcp 形式调用的 remote ejb。
+        properties.put(Context.PROVIDER_URL, "ejbd://127.0.0.1:4201");
+        properties.put(Context.INITIAL_CONTEXT_FACTORY, "org.apache.openejb.client.RemoteInitialContextFactory");
+
+        //不是使用 openejb.client.moduleId 属性，则 tomee 的 server 端执行默认在 "openejb/remote/" 下面的检索
+        //这个方式和将 moduleId 设置为 "openejb/global" 时对于 global 资源定位是等效的，remote 和 global 都有 jndi 子树 global。
+        //参考 org.apache.openejb.server.ejbd.JndiRequestHandler#getPrefix
+        //properties.setProperty("openejb.client.moduleId", "stateful_remove_annotated_client");
+
+        //此时资源的名字就是 ejb bean 的部署模块名，bean名以及接口信息的联合。
         InitialContext initialContext = new InitialContext(properties);
         RemoveIF lookup = (RemoveIF) initialContext.
                 lookup("global/stateful_remove_annotated/stateful_remove_annotated_ejb/RemoveBean!com.sun.ts.tests.ejb30.bb.session.stateful.remove.common.RemoveIF");
@@ -91,19 +98,25 @@ public class StatelessTestBean extends TestBeanBase implements TestIF {
 
     @Override
     public Remove2IF getRemoveRemoteBean2Return() {
+        //这里是在 server 端执行的 bean 查询，所以其名字使用的是 server 端 ejb 模块通过 DD 或者 @EJB 注解定义的名字
         return (Remove2IF) sessionContext.lookup("removeBean2Remote");
     }
 
     @Override
     public Remove2IF getRemoveRemoteBean2ByRemoteCtxReturn() throws NamingException {
         Properties properties = new Properties();
+
+        //这里的 url 使用了 http 形式调用的 remote ejb。
         properties.put(Context.PROVIDER_URL, "http://127.0.0.1:8080/tomee/ejb");
         properties.put(Context.INITIAL_CONTEXT_FACTORY, "org.apache.openejb.client.RemoteInitialContextFactory");
-        properties.setProperty("openejb.client.moduleId", "openejb/global");
 
+        //这里使用的是标准的 openejb.client.moduleId 属性，其值是一个部署在 server 上的 client 模块的模块名。
+        //可以通过这个 moduleId 来获取在 server 上部署的指定 client 模块对应的 jndi 子树上面的资源。
+        properties.setProperty("openejb.client.moduleId", "stateful_remove_annotated_client");
+
+        //其资源名就是 client 应用中通过 DD 或者是 @EJB 注解定义的资源。
         InitialContext initialContext = new InitialContext(properties);
-        Remove2IF lookup = (Remove2IF) initialContext.
-                lookup("global/stateful_remove_annotated/stateful_remove_annotated_ejb/RemoveBean!com.sun.ts.tests.ejb30.bb.session.stateful.remove.common.Remove2IF");
+        Remove2IF lookup = (Remove2IF) initialContext.lookup("comp/env/removeBean2");
         return lookup;
     }
 
@@ -126,7 +139,7 @@ public class StatelessTestBean extends TestBeanBase implements TestIF {
         //而是直接取巧的使用了 global 作为 moduleId 来使用 server 端 jndi 树上的 openejb/global 子树，其包含了所有 global 域中的资源
         properties.setProperty("openejb.client.moduleId", "openejb/global");
 
-        //这里的信息来源于下面的类方法
+        //此时资源的名字就是 ejb bean 的部署模块名，bean名以及接口信息的联合，这里的信息来源于下面的类方法
         //org.apache.openejb.server.ejbd.JndiRequestHandler#getPrefix
         //org.apache.openejb.server.ejbd.JndiRequestHandler#doLookup
         InitialContext initialContext = new InitialContext(properties);
