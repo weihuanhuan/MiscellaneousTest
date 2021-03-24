a6e12d2ced13a986f592ae6a67c2edc8c8128bc6
weihuanhuan1107 <weihuanhuan1107@126.com>
2021-03-24 14:17:02 +0800

    using library explictly by System and Runtime

--- feature/src/main/java/SystemTest/LdLibraryPathTest.java
+++ feature/src/main/java/SystemTest/LdLibraryPathTest.java
@@ -10,13 +10,19 @@ public class LdLibraryPathTest {
         String pathSeparator = File.pathSeparator;
 
         System.out.println("################# testJavaLibraryPath ##########################");
+        //java command argument for both windows(dll) and linux(so).
         testJavaLibraryPath(pathSeparator);
 
         System.out.println("################# testEnvPath ##########################");
+        // environment variable for windows dll
         testEnvPath(pathSeparator);
 
         System.out.println("################# testEnvLdLibraryPath ##########################");
+        // environment variable for linux so
         testEnvLdLibraryPath(pathSeparator);
+
+        // using library explicitly
+        testExplicitLoadLibrary();
     }
 
     public static void testJavaLibraryPath(String delimiter) {
@@ -32,6 +38,13 @@ public class LdLibraryPathTest {
     public static void testEnvLdLibraryPath(String delimiter) {
         String ldLibraryLibEnv = System.getenv("LD_LIBRARY_LIB");
         printProperty(ldLibraryLibEnv, delimiter);
+    }
+
+    public static void testExplicitLoadLibrary() {
+//        System.load("filename");
+//        System.loadLibrary("libName");
+//        Runtime.getRuntime().load("filename");
+//        Runtime.getRuntime().loadLibrary("libName");
     }
 
     public static void printProperty(String property, String delimiter) {
