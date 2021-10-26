094f730de686917f663b51592fc966c1e367e00e
JasonFitch <weihuanhuan1107@126.com>
2021-10-26 22:51:23 +0800

    springmvc: test <ResourceLink> element form context.xml of tomcat.

--- springmvc/src/main/java/Servlet/DatasourceTest.java
+++ springmvc/src/main/java/Servlet/DatasourceTest.java
@@ -1,9 +1,5 @@
 package Servlet;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.SQLException;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
@@ -13,48 +9,67 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 /**
  * Created by JasonFitch on 7/2/2018.
  */
-@WebServlet(urlPatterns = "/datasource",name = "datasource")
-public class DatasourceTest extends HttpServlet
-{
+@WebServlet(urlPatterns = "/datasource", name = "datasource")
+public class DatasourceTest extends HttpServlet {
+
     @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
-    {
-        try
-        {
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String prefix = "java:comp/env/";
+
+        String globalName = "jdbc/Server";
+        lookupTest(prefix + globalName);
+
+        String contextName = "jdbc/Context";
+        lookupTest(prefix + contextName);
+
+        String linkName = "jdbc/Reference";
+        lookupTest(prefix + linkName);
+
+        String factoryName = "jdbc/Hikari";
+        lookupTest(prefix + factoryName);
+
+        //这里我们去掉了前缀，试图查找全局的资源。
+        lookupTest(globalName);
+
+        lookupTest(contextName);
+
+        lookupTest(linkName);
+
+        lookupTest(factoryName);
+    }
+
+    private void lookupTest(String name) {
+        try {
             Context initContext = new InitialContext();
-//            Context envContext  = null;
-//            envContext = (Context)initContext.lookup("java:/comp/env");
-//            DataSource datasource = (DataSource)initContext.lookup("java:/comp/env/jdbc/Context");
-
-//            DataSource datasource = (DataSource)initContext.lookup("java:/comp/env/jdbc/Server");
-
-            DataSource datasource = (DataSource)initContext.lookup("java:/comp/env/jdbc/Hikari");
-
-//            DataSource datasource = (DataSource)initContext.lookup("java:/comp/env/jdbc/Reference");
+            DataSource datasource = (DataSource) initContext.lookup(name);
 
             Connection con = datasource.getConnection();
-            if(null ==con)
-            {
-                System.out.println("null");
-
-            }
-            else
-            {
-                PrintWriter pw = resp.getWriter();
-                pw.println(con.toString());
-            }
-
-        } catch (NamingException e)
-        {
-            e.printStackTrace();
-        } catch (SQLException e)
-        {
-            e.printStackTrace();
+            System.out.println(String.format("[%s] lookup result is [%s].", name, con == null ? "null" : con.toString()));
+        } catch (NamingException | SQLException e) {
+            System.out.println(String.format("[%s] lookup exception is:", name));
+            System.out.println(exceptionToString(e));
         }
-
     }
+
+    private String exceptionToString(Exception e) {
+        StringWriter stringWriter = new StringWriter();
+        PrintWriter printWriter = new PrintWriter(stringWriter);
+
+        e.printStackTrace(printWriter);
+        printWriter.flush();
+
+        StringBuffer buffer = stringWriter.getBuffer();
+        return buffer.toString();
+    }
+
 }
