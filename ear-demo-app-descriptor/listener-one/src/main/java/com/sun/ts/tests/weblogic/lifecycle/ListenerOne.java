5601b71e8ae6f4e6043da853573b477a426afad6
JasonFitch <weihuanhuan1107@126.com>
2021-11-26 19:27:21 +0800

    ear-demo-app-descriptor: add weblogic-application.xml lifecycle listener test, provides implementation of listener

--- ear-demo-app-descriptor/listener-one/src/main/java/com/sun/ts/tests/weblogic/lifecycle/ListenerOne.java
+++ ear-demo-app-descriptor/listener-one/src/main/java/com/sun/ts/tests/weblogic/lifecycle/ListenerOne.java
@@ -1,4 +1,41 @@
 package com.sun.ts.tests.weblogic.lifecycle;
 
-public class ListenerOne {
+import org.glassfish.api.deployment.lifecycle.ApplicationContext;
+import org.glassfish.api.deployment.lifecycle.ApplicationLifecycleListener;
+import org.glassfish.api.deployment.lifecycle.ApplicationLifecycleEvent;
+import org.glassfish.api.deployment.lifecycle.ApplicationLifecycleException;
+import org.glassfish.api.deployment.lifecycle.DeploymentOperationType;
+
+public class ListenerOne implements ApplicationLifecycleListener {
+
+    private Integer value = 0;
+
+    public void preStart(ApplicationLifecycleEvent event) throws ApplicationLifecycleException {
+        System.out.println(String.format("ListenerOne#preStart %s", ++value));
+        printEventInfo(event);
+    }
+
+    public void postStart(ApplicationLifecycleEvent event) throws ApplicationLifecycleException {
+        System.out.println(String.format("ListenerOne#postStart %s", ++value));
+        printEventInfo(event);
+    }
+
+    public void preStop(ApplicationLifecycleEvent event) throws ApplicationLifecycleException {
+        System.out.println(String.format("ListenerOne#preStop %s", ++value));
+        printEventInfo(event);
+    }
+
+    public void postStop(ApplicationLifecycleEvent event) throws ApplicationLifecycleException {
+        System.out.println(String.format("ListenerOne#postStop %s", ++value));
+        printEventInfo(event);
+    }
+
+    private void printEventInfo(ApplicationLifecycleEvent event) {
+        DeploymentOperationType deploymentOperation = event.getDeploymentOperation();
+        ApplicationContext applicationContext = event.getApplicationContext();
+        System.out.println(String.format("deploymentOperation: %s", deploymentOperation));
+        System.out.println(String.format("applicationContext.getApplicationName(): %s", applicationContext.getApplicationName()));
+        System.out.println(String.format("applicationContext.getAppClassLoader(): %s", applicationContext.getAppClassLoader()));
+    }
+
 }
--- ear-demo-app-descriptor/listener-two/src/main/java/com/sun/ts/tests/weblogic/lifecycle/ListenerTwo.java
+++ ear-demo-app-descriptor/listener-two/src/main/java/com/sun/ts/tests/weblogic/lifecycle/ListenerTwo.java
@@ -1,4 +1,41 @@
 package com.sun.ts.tests.weblogic.lifecycle;
 
-public class ListenerTwo {
+import org.glassfish.api.deployment.lifecycle.ApplicationContext;
+import org.glassfish.api.deployment.lifecycle.ApplicationLifecycleListener;
+import org.glassfish.api.deployment.lifecycle.ApplicationLifecycleEvent;
+import org.glassfish.api.deployment.lifecycle.ApplicationLifecycleException;
+import org.glassfish.api.deployment.lifecycle.DeploymentOperationType;
+
+public class ListenerTwo implements ApplicationLifecycleListener {
+
+    private Integer value = 0;
+
+    public void preStart(ApplicationLifecycleEvent event) throws ApplicationLifecycleException {
+        System.out.println(String.format("ListenerTwo#preStart %s", ++value));
+        printEventInfo(event);
+    }
+
+    public void postStart(ApplicationLifecycleEvent event) throws ApplicationLifecycleException {
+        System.out.println(String.format("ListenerTwo#postStart %s", ++value));
+        printEventInfo(event);
+    }
+
+    public void preStop(ApplicationLifecycleEvent event) throws ApplicationLifecycleException {
+        System.out.println(String.format("ListenerTwo#preStop %s", ++value));
+        printEventInfo(event);
+    }
+
+    public void postStop(ApplicationLifecycleEvent event) throws ApplicationLifecycleException {
+        System.out.println(String.format("ListenerTwo#postStop %s", ++value));
+        printEventInfo(event);
+    }
+
+    private void printEventInfo(ApplicationLifecycleEvent event) {
+        DeploymentOperationType deploymentOperation = event.getDeploymentOperation();
+        ApplicationContext applicationContext = event.getApplicationContext();
+        System.out.println(String.format("deploymentOperation: %s", deploymentOperation));
+        System.out.println(String.format("applicationContext.getApplicationName(): %s", applicationContext.getApplicationName()));
+        System.out.println(String.format("applicationContext.getAppClassLoader(): %s", applicationContext.getAppClassLoader()));
+    }
+
 }
