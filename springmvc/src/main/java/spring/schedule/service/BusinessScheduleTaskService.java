836bde0b5abf4b4148634dd8530184ad872201eb
JasonFitch <weihuanhuan1107@126.com>
2022-06-16 23:58:26 +0800

    springmvc: test spring job schedule, fixed stream collection ConcurrentModificationException.

--- springmvc/src/main/java/spring/schedule/service/BusinessScheduleTaskService.java
+++ springmvc/src/main/java/spring/schedule/service/BusinessScheduleTaskService.java
@@ -6,7 +6,7 @@ import spring.schedule.entity.BusinessBean;
 import spring.schedule.scheduling.ScheduleTask;
 import spring.schedule.scheduling.ScheduleTaskService;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -22,11 +22,12 @@ public class BusinessScheduleTaskService extends ScheduleTaskService<BusinessBea
     @Override
     protected List<ScheduleTask<BusinessBean>> findScheduleTask() {
         List<BusinessBean> businessBeans = businessService.getBusinessBeans();
-        if (businessBeans == null || businessBeans.isEmpty()) {
-            return Collections.emptyList();
-        }
 
-        return businessBeans.stream()
+        //avoid ConcurrentModificationException
+        List<BusinessBean> copy = new ArrayList<>(businessBeans);
+
+        //filter
+        return (copy).stream()
                 .filter(businessBean -> businessService.isValid(businessBean))
                 .map(builder::build)
                 .collect(Collectors.toList());
