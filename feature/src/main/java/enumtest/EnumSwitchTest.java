3711387b1dc678a80e320b0e1e5a950f7ea404d7
JasonFitch <weihuanhuan1107@126.com>
2022-08-06 10:58:56 +0800

    feature: test for switch statement with null enum value.

---
+++ feature/src/main/java/enumtest/EnumSwitchTest.java
@@ -0,0 +1,22 @@
+package enumtest;
+
+/**
+ * Created by JasonFitch on 12/19/2018.
+ */
+public class EnumSwitchTest {
+
+    public static void main(String[] args) {
+
+        EnumTest.InnerEnum innerEnum = null;
+
+        //Exception in thread "main" java.lang.NullPointerException
+        switch (innerEnum) {
+            case APPLE:
+            case ORANGE:
+            case DUMMY:
+            default:
+        }
+    }
+
+}
+
