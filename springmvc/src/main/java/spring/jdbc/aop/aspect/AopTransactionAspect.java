a285d78de42d149c583b4ca4b1b6e328f784a4df
JasonFitch <weihuanhuan1107@126.com>
2023-07-25 21:09:22 +0800

    springmvc: test get parameters within advice using org.aspectj.lang.JoinPoint.

--- springmvc/src/main/java/spring/jdbc/aop/aspect/AopTransactionAspect.java
+++ springmvc/src/main/java/spring/jdbc/aop/aspect/AopTransactionAspect.java
@@ -1,18 +1,29 @@
 package spring.jdbc.aop.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.support.TransactionSynchronizationAdapter;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
 
+import java.util.Arrays;
+
 @Component
 @Aspect
 class AopTransactionAspect extends TransactionSynchronizationAdapter {
 
     @Before("@annotation(org.springframework.transaction.annotation.Transactional)")
-    public void registerSynchronization() {
+    public void registerSynchronization(JoinPoint joinPoint) {
         System.out.println(this.getClass().getSimpleName() + ": registerSynchronization");
+
+        //我们也可以通过向 @Before advice 的方法中添加 org.aspectj.lang.JoinPoint 参数对象，并从中获取被 aspect 的方法参数
+        Object[] args = joinPoint.getArgs();
+        Object target = joinPoint.getTarget();
+        System.out.println(this.getClass().getSimpleName() + ": joinPoint=" + joinPoint);
+        System.out.println(this.getClass().getSimpleName() + ": target=" + target);
+        System.out.println(this.getClass().getSimpleName() + ": args=" + Arrays.deepToString(args));
+
         TransactionSynchronizationManager.registerSynchronization(this);
     }
 
