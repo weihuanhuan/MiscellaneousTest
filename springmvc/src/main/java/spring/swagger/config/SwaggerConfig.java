6996d9679e9252f75361e65f9c7f60765d6413ea
JasonFitch <weihuanhuan1107@126.com>
2023-09-08 13:23:59 +0800

    springmvc: support for using swagger2, control the loading of swagger based @PropertySource with @Conditional.

--- springmvc/src/main/java/spring/swagger/config/SwaggerConfig.java
+++ springmvc/src/main/java/spring/swagger/config/SwaggerConfig.java
@@ -2,7 +2,8 @@ package spring.swagger.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.annotation.Order;
 import spring.swagger.condition.SwaggerInitialize;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -15,47 +16,59 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import java.util.Collections;
 
 @Configuration
-@EnableSwagger2
-// 分离 SwaggerConfig 和 TestControllerConfig ，使得在 swagger 禁用时，依旧可以加载 controller
-// -Dspring.profiles.active=swagger
-// @Profile({"!production && swagger"})
-// 使用独立的注解来控制 swagger api 的加载，避免使用影响面较大的 @Profile
-// -Dinitialize.swagger.allowed=true
-@SwaggerInitialize
 public class SwaggerConfig {
 
-    static {
-        String activeProperty = System.getProperty("spring.profiles.active");
-        System.out.println("############## " + "spring.profiles.active=" + activeProperty + " ##############");
-        String allowedProperty = System.getProperty("initialize.swagger.allowed");
-        System.out.println("############## " + "initialize.swagger.allowed=" + allowedProperty + " ##############");
+    // 对于 @PropertySource 类，需要手动添加 @Configuration ，否则内部类不会被作为配置类处理
+    // 而 @EnableSwagger2 自己 @Import({Swagger2DocumentationConfiguration.class}) 的中添加了 @Configuration
+    @Configuration
+    @PropertySource(value = "classpath:spring-swagger.properties", ignoreResourceNotFound = true)
+    @Order(-1)
+    public static class InitializeProperty {
     }
 
-    @Bean
-    public Docket api() {
-        //http://127.0.0.1:8080/springmvc/swagger-ui.html
-        Docket docket = new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .paths(PathSelectors.ant("/swagger-test/*"))
-                .apis(RequestHandlerSelectors.basePackage("spring.swagger.controller"))
-                .build()
-                .apiInfo(appInfo());
+    @EnableSwagger2
+    // 分离 SwaggerConfig 和 TestControllerConfig ，使得在 swagger 禁用时，依旧可以加载 controller
+    // -Dspring.profiles.active=swagger
+    // @Profile({"!production && swagger"})
+    // 使用独立的注解来控制 swagger api 的加载，避免使用影响面较大的 @Profile
+    // -Dinitialize.swagger.allowed=true
+    @SwaggerInitialize
+    @Order(0)
+    public static class InitializeSwagger {
 
-        System.out.println("############## " + "initialized swagger api!" + " ##############");
-        return docket;
-    }
+        static {
+            String activeProperty = System.getProperty("spring.profiles.active");
+            System.out.println("############## " + "spring.profiles.active=" + activeProperty + " ##############");
+            String allowedProperty = System.getProperty("initialize.swagger.allowed");
+            System.out.println("############## " + "initialize.swagger.allowed=" + allowedProperty + " ##############");
+        }
 
-    private ApiInfo appInfo() {
-        return new ApiInfo(
-                "Demo api",
-                "Sample api",
-                "1.0",
-                "http://localhost/termsOfService",
-                new Contact("demo", "https://demo.com", "demo@gmail.com"),
-                "demo license",
-                "https://demo-license",
-                Collections.emptyList()
-        );
+        @Bean
+        public Docket api() {
+            //http://127.0.0.1:8080/springmvc/swagger-ui.html
+            Docket docket = new Docket(DocumentationType.SWAGGER_2)
+                    .select()
+                    .paths(PathSelectors.ant("/swagger-test/*"))
+                    .apis(RequestHandlerSelectors.basePackage("spring.swagger.controller"))
+                    .build()
+                    .apiInfo(appInfo());
+
+            System.out.println("############## " + "initialized swagger api!" + " ##############");
+            return docket;
+        }
+
+        private ApiInfo appInfo() {
+            return new ApiInfo(
+                    "Demo api",
+                    "Sample api",
+                    "1.0",
+                    "http://localhost/termsOfService",
+                    new Contact("demo", "https://demo.com", "demo@gmail.com"),
+                    "demo license",
+                    "https://demo-license",
+                    Collections.emptyList()
+            );
+        }
     }
 
 }
